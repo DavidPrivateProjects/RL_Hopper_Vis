7c0e988b503c61188dc2be55471c91be4a300f3e
david-petrovic <david.petrovic@akane.waseda.jp>
2025-04-29 04:09:48 +0200

    feat: Improve training code and training procedure

--- main.py
+++ main.py
@@ -2,10 +2,25 @@ import gymnasium as gym
 from stable_baselines3 import SAC, TD3, A2C
 import os
 import argparse
+import numpy as np
+from collections import OrderedDict
+
+class ZeroOutActionsWrapper(gym.ActionWrapper):
+    def __init__(self, env):
+        super().__init__(env)
+        self.action_space = env.action_space  # Keep the action space unchanged
+
+    def action(self, action):
+        action = np.array(action, dtype=np.float32)
+        action[11:17] = 0.0  # Set actions 11 to 16 to zero
+        return action
+
 
 # HumanoidStandup-v5
 # python main.py HumanoidStandup-v5 SAC -s ./models/SAC_125000.zip
-
+# python main.py HumanoidStandup-v5 SAC -t
+# 
+# https://www.youtube.com/watch?v=OqvXHi_QtT0
 
 # Create directories to hold models and logs
 model_dir = "models"
@@ -17,7 +32,7 @@ os.makedirs(log_dir, exist_ok=True)
 def get_model(algo, env):
     """Returns the appropriate model based on the algorithm."""
     if algo == 'SAC':
-        return SAC('MlpPolicy', env, verbose=1, device='cuda', tensorboard_log=log_dir)
+         return SAC('MlpPolicy', env, verbose=1, device='cuda', tensorboard_log=log_dir)
     elif algo == 'TD3':
         return TD3('MlpPolicy', env, verbose=1, device='cuda', tensorboard_log=log_dir)
     elif algo == 'A2C':
@@ -33,12 +48,14 @@ def train(env, algo):
     if not model:
         return
 
-    TIMESTEPS = 25000
+    TIMESTEPS = 50000
     iters = 0
     while True:
         iters += 1
         model.learn(total_timesteps=TIMESTEPS, reset_num_timesteps=False)
         model.save(f"{model_dir}/{algo}_{TIMESTEPS*iters}")
+        if iters > 5:
+            break
 
 
 def test(env, algo, path_to_model):
@@ -57,34 +74,39 @@ def test(env, algo, path_to_model):
         return
 
     obs = env.reset()[0]
-    done = False
-    extra_steps = 500
+    extra_steps = 50
     while True:
         action, _ = model.predict(obs)
-        obs, _, _, _, _ = env.step(action)
-
-        extra_steps -= 1
-        if extra_steps < 0:
+        obs, _, terminated, _, _ = env.step(action)
+        
+        if terminated==True:
+            extra_steps -= 1
+        
+        if extra_steps == 0:
             obs = env.reset()[0]
-            extra_steps = 500
+            extra_steps = 50
 
 
 if __name__ == '__main__':
     # Parse command line inputs
     parser = argparse.ArgumentParser(description='Train or test model.')
     parser.add_argument('gymenv', help='Gymnasium environment i.e. Humanoid-v4')
-    parser.add_argument('sb3_algo', help='StableBaseline3 RL algorithm i.e. SAC, TD3')
+    parser.add_argument('algo', help='StableBaseline3 RL algorithm i.e. SAC, TD3')
     parser.add_argument('-t', '--train', action='store_true')
     parser.add_argument('-s', '--test', metavar='path_to_model')
     args = parser.parse_args()
 
     if args.train:
         gymenv = gym.make(args.gymenv, render_mode=None)
-        train(gymenv, args.sb3_algo)
+        gymenv = ZeroOutActionsWrapper(gymenv)
+        
+        train(gymenv, args.algo)
 
     if args.test:
         if os.path.isfile(args.test):
             gymenv = gym.make(args.gymenv, render_mode='human')
-            test(gymenv, args.sb3_algo, path_to_model=args.test)
+            gymenv = ZeroOutActionsWrapper(gymenv)
+            
+            test(gymenv, args.algo, path_to_model=args.test)
         else:
             print(f'{args.test} not found.')
